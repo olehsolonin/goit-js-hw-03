8c3321fccdf66a89f9ea6b41962834fb868c66be
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-03-11 20:33:17 +0200

    HW3

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,24 @@
-function checkForSpam(message) {
-	const spam = "spam";
-	const sale = "sale";
-	let messageLow = message.toLowerCase();
+function filterArray(numbers, value) {
 
-	if (messageLow.includes(spam) || messageLow.includes(sale)) {
-		return true;
+	let resultArray = [];
+
+	for (const i of numbers) {
+		if (i > value) {
+			resultArray.push(i);
+		}
 	}
-	else {
-		return false;
-	}
+
+	return resultArray;
+
+
 
 }
 
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
